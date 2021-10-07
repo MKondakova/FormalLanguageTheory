905b4ea6e83e6c2f148fa4496a1b3e26e7c7422b
MKondakova <milanon@ya.ru>
2021-10-07 23:58:05 +0300

    init

---
+++ lab1/trs_unification.js
@@ -0,0 +1,132 @@
+const fs = require('fs');
+const { resourceUsage } = require('process');
+
+let error = null;
+let constructors = [];
+let variables = [];
+let first = '';
+let second = '';
+const VAR = 'variable';
+const CONSTR = 'constructor';
+
+function Init(path) {
+	let input = '';
+	try {
+		input = fs.readFileSync(path, 'utf8');
+	} catch (err) {
+		error = err.toString();
+		return
+	}
+	input = input.split(/\r\n|\r|\n/);
+	getConstructors(input[0]);
+	if (error) {
+		return
+	}
+	getVariables(input[1]);
+	if (error) {
+		return
+	}
+	getFirstAndSecond(input[2], input[3]);
+	if (error) {
+		return
+	}
+	
+
+}
+
+function getConstructors(line) {
+	let constructorsStr = line.match(/constructors[ \t]*=[ \t]*(.*)/)[1];
+	let constructorsArr = constructorsStr.replace(/[\(]|[\)\s*,?]/g, ' ').split(/ +/);
+	if (constructorsArr[constructorsArr.length - 1] === '') {
+		constructorsArr.pop()
+	}
+	constructorsArr.forEach((v, i, a) => {
+		if (error) {
+			return;
+		}
+		if (i % 2 == 0) {
+			if (v in constructors) {
+				error = 'Неверный формат конструкторов, конструкторы не уникальны.';
+			}
+			constructors[v] = { 'name': v, 'args': parseInt(a[i+1]) };
+		}
+	});
+}
+
+function getVariables(line) {
+	let variablesStr = line.match(/variables[ \t]*=[ \t]*(.*)/)[1];
+	variables = variablesStr.split(',');
+}
+
+function getFirstAndSecond(line1, line2) {
+	first = line1.match(/first[ \t]*=[ \t]*(.*)/)[1];
+	second = line2.match(/second[ \t]*=[ \t]*(.*)/)[1];
+}
+
+function parseTerm(expr, pos) {
+	if (error) {
+		return null;
+	}
+	console.log(expr.slice(pos), pos);
+	pos = skipSpace(expr, pos);
+	if (!/^[0-9a-zA-Zа-яА-Я_-].*/.test(expr.split(pos))) {
+		error = "Ошибка при обработке терма " + expr + " на позиции " + pos + ". Некорректный символ";
+		return null;
+	}
+	const endOfName = getNameEnd(expr, pos);
+	const name = expr.slice(pos, endOfName);
+	pos = endOfName;
+
+	if (variables.includes(name)) {
+		return { name, 'type': VAR, pos };
+	} else if (name in constructors) {
+		if ((expr[pos] !== '(' && constructors[name].args !== 0) ||
+			(expr[pos] === '(' && constructors[name].args === 0)) {
+			error = "Ошибка при обработке терма " + expr + " на позиции " + pos + ". Неверное количество аргументов у " + name;
+			return null;
+		}
+		const result = { name, 'type': CONSTR, 'args': []}
+		if (constructors[name].args === 0) {
+			result['pos'] = pos;
+			return result;
+		}
+		pos ++;
+		while (expr[pos] !== ')') {
+			const argParseResult = parseTerm(expr, pos);
+			if (error) {
+				return null;
+			}
+			pos = argParseResult.pos;
+			pos = skipSpace(expr, pos);
+			if (expr[pos] !== ',' && expr[pos] !== ')'){
+				error = "Ошибка при обработке терма " + expr + " на позиции " + pos + ". Неверный формат аргументов у " + name;
+				return null;
+			}
+			if (expr[pos] === ',') pos++;
+			result.args.push(argParseResult);
+		}
+		if (result.args.length !== constructors[name].args) {
+			error = "Ошибка при обработке терма " + expr + " на позиции " + pos + ". Неверное количество аргументов у " + name;
+			return null;	
+		}
+		pos++;
+		result['pos'] = pos;
+		return result;
+	} else {
+		error = "Ошибка при обработке терма " + expr + " на позиции " + pos + ". Имя не найдено";
+		return null;
+	}
+}
+
+function getNameEnd(line, start) {
+	return line.slice(start).search(/[\(\), ]/) + start;
+}
+
+function skipSpace(line, start) {
+	return line.slice(start).search(/\S/) + start;
+}
+
+Init('test1.txt');
+first = parseTerm(first, 0);
+second = parseTerm(second, 0);
+console.log(first, second, error);
